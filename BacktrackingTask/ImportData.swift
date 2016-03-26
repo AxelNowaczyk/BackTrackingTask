c5f9d34e03e0b89361bfc7475e99871b2d0f997a
Axel Nowaczyk <axelnowaczyk@gmail.com>
2016-03-26 22:51:44 +0100

    importing data from net completed

--- BacktrackingTask/ImportData.swift
+++ BacktrackingTask/ImportData.swift
@@ -9,5 +9,17 @@
 import Foundation
 
 func importData(){
+    let URLString = "https://www.kilgarriff.co.uk/BNClists/lemma.al"
+    guard let myURL = NSURL(string: URLString) else {
+        print("Error: \(URLString) ")
+        return
+    }
+    
+    do {
+        let HTMLString = try String(contentsOfURL: myURL)
+        print("\(HTMLString)") 
+    } catch let error as NSError {
+        print("Error: \(error)")
+    }
     print("importing data")
 }
