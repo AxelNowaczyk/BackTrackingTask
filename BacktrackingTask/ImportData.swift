28c01335ae577394bfc8342de718ac25f7e507bd
Axel Nowaczyk <axelnowaczyk@gmail.com>
2016-03-26 21:06:13 +0100

    initial commit

---
+++ BacktrackingTask/ImportData.swift
@@ -0,0 +1,13 @@
+//
+//  ImportData.swift
+//  BacktrackingTask
+//
+//  Created by Axel Nowaczyk on 26.03.2016.
+//  Copyright © 2016 Axel Nowaczyk. All rights reserved.
+//
+
+import Foundation
+
+func importData(){
+    print("importing data")
+}
